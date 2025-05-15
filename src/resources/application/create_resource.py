2ef5c0095747e7ecd4c089546e25d6529a8c07a6
SantiPorres <sporres09@gmail.com>
2025-05-14 23:47:49 -0500

    test for creating resource with not valid url

--- src/resources/application/create_resource.py
+++ src/resources/application/create_resource.py
@@ -1,9 +1,18 @@
+from dataclasses import dataclass
+
+from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
+
+
+@dataclass
+class CreateResourceCommand:
+    resource_url: str
 
 
 class CreateResource:
     def __init__(self, resource_repository: ResourcesRepository) -> None:
-        pass
+        self._resource_repository = resource_repository
 
-    def execute(self) -> None:
-        pass
+    def execute(self, command: CreateResourceCommand) -> None:
+        resource = Resource.create(resource_url=command.resource_url)
+        self._resource_repository.save(resource=resource)
--- src/resources/tests/application/test_create_resource.py
+++ src/resources/tests/application/test_create_resource.py
@@ -1,4 +1,7 @@
-from resources.application.create_resource import CreateResource
+import pytest
+
+from resources.application.create_resource import CreateResource, CreateResourceCommand
+from resources.domain.exceptions import UrlIsNotValid
 from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
 
@@ -17,6 +20,22 @@ class FakeResourceRepository(ResourcesRepository):
 class TestCreateResource:
     def test_create_resource(self) -> None:
         resource_repository = FakeResourceRepository()
-        CreateResource(resource_repository).execute()
+        CreateResource(resource_repository).execute(
+            command=CreateResourceCommand(
+                resource_url="https://google.com",
+            )
+        )
+        resources = resource_repository.all()
+        assert len(resources) == 1
+        assert resources[0].url() == "https://google.com"
 
-        assert len(resource_repository.all()) == 1
+    def test_raise_exception_when_resource_url_is_not_a_valid_url(self) -> None:
+        resource_repository = FakeResourceRepository()
+        with pytest.raises(UrlIsNotValid):
+            CreateResource(resource_repository).execute(
+                command=CreateResourceCommand(
+                    resource_url="not-a-valid-url",
+                )
+            )
+        resources = resource_repository.all()
+        assert len(resources) == 0
